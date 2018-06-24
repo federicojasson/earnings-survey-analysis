a02ac1811a69bdd6dfdeca525f02d9a1095f9152
Federico Jasson <federicojasson@gmail.com>
2018-06-23 21:19:48 -0300

    Fix run script bugs

--- bin/run.sh
+++ bin/run.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-root_dir="$(dirname "${0}")/.."
+root_dir="$(cd "$(dirname "${0}")/.." && pwd)"
 src_dir="${root_dir}/src"
 
-docker run -d -p 8888:8888 -v "${src_dir}":/home/jovyan jupyter/r-notebook:03b897d05f16
+docker run -it \
+    -p 8888:8888 \
+    -v "${src_dir}":/home/jovyan \
+    jupyter/r-notebook:03b897d05f16
